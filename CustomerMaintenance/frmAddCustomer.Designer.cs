7ce63cd2d698465eaaeaebc9459effe10e11e670
Greg Doud <gdoud@gmail.com>
2022-02-03 20:06:18 -0500

    Complete

--- CustomerMaintenance/frmAddCustomer.Designer.cs
+++ CustomerMaintenance/frmAddCustomer.Designer.cs
@@ -40,51 +40,57 @@
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(83, 76);
+            this.txtEmail.Location = new System.Drawing.Point(111, 117);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(176, 20);
+            this.txtEmail.Size = new System.Drawing.Size(233, 27);
             this.txtEmail.TabIndex = 15;
             this.txtEmail.Tag = "Email";
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(83, 44);
+            this.txtLastName.Location = new System.Drawing.Point(111, 68);
+            this.txtLastName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtLastName.Name = "txtLastName";
-            this.txtLastName.Size = new System.Drawing.Size(176, 20);
+            this.txtLastName.Size = new System.Drawing.Size(233, 27);
             this.txtLastName.TabIndex = 14;
             this.txtLastName.Tag = "Last Name";
             // 
             // txtFirstName
             // 
-            this.txtFirstName.Location = new System.Drawing.Point(83, 12);
+            this.txtFirstName.Location = new System.Drawing.Point(111, 18);
+            this.txtFirstName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtFirstName.Name = "txtFirstName";
-            this.txtFirstName.Size = new System.Drawing.Size(176, 20);
+            this.txtFirstName.Size = new System.Drawing.Size(233, 27);
             this.txtFirstName.TabIndex = 13;
             this.txtFirstName.Tag = "First Name";
             // 
             // label4
             // 
-            this.label4.Location = new System.Drawing.Point(11, 76);
+            this.label4.Location = new System.Drawing.Point(15, 117);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(72, 23);
+            this.label4.Size = new System.Drawing.Size(96, 35);
             this.label4.TabIndex = 20;
             this.label4.Text = "Email:";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(11, 44);
+            this.label3.Location = new System.Drawing.Point(15, 68);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 23);
+            this.label3.Size = new System.Drawing.Size(96, 35);
             this.label3.TabIndex = 19;
             this.label3.Text = "Last Name:";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(11, 12);
+            this.label2.Location = new System.Drawing.Point(15, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 23);
+            this.label2.Size = new System.Drawing.Size(96, 35);
             this.label2.TabIndex = 18;
             this.label2.Text = "First Name:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -92,27 +98,31 @@
             // btnCancel
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(182, 107);
+            this.btnCancel.Location = new System.Drawing.Point(243, 165);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(100, 35);
             this.btnCancel.TabIndex = 17;
             this.btnCancel.Text = "&Cancel";
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(101, 107);
+            this.btnSave.Location = new System.Drawing.Point(135, 165);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.Size = new System.Drawing.Size(100, 35);
             this.btnSave.TabIndex = 16;
             this.btnSave.Text = "&Save";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // frmAddCustomer
             // 
             this.AcceptButton = this.btnSave;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(272, 147);
+            this.ClientSize = new System.Drawing.Size(363, 226);
             this.ControlBox = false;
             this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.txtLastName);
@@ -123,12 +133,14 @@
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "frmAddCustomer";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Add Customer";
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
--- CustomerMaintenance/frmAddCustomer.cs
+++ CustomerMaintenance/frmAddCustomer.cs
@@ -10,9 +10,38 @@ namespace CustomerMaintenance
 {
     public partial class frmAddCustomer : Form
     {
+        private Customer customer = null;
+
+        public Customer GetNewCustomer() {
+            ShowDialog();
+            return customer;
+        }
         public frmAddCustomer()
         {
             InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e) {
+            if(!IsValidData()) {
+                return;
+            }
+            customer = new Customer(txtFirstName.Text, txtLastName.Text, txtEmail.Text);
+            Close();
+        }
+        private bool IsValidData() { 
+            string message = string.Empty;
+            message += Validator.IsPresent(txtFirstName.Text, txtFirstName.Tag.ToString());
+            message += Validator.IsPresent(txtLastName.Text, txtLastName.Tag.ToString());
+            message += Validator.IsValidEmail(txtEmail.Text, txtEmail.Tag.ToString());
+            if(message.Length > 0) {
+                MessageBox.Show(message, "Data Errors");
+                return false;
+            }
+            return true;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e) {
+            Close();
         }
     }
 }
