6f938f5f2e1b276f2db6e6def59302d04ef82f1f
Greg Doud <gdoud@gmail.com>
2022-02-03 19:40:58 -0500

    Completed

---
+++ CustomerMaintenance/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerMaintenance {
+    public class Customer {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public string GetDisplayText() => $"{FirstName} {LastName}, {Email}";
+
+        public Customer() { }
+
+        //public Customer(string firstName, string lastName, string email) => 
+        //    (FirstName, LastName, Email) = (FirstName, lastName, email);
+
+        public Customer(string firstName, string lastName, string email) {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+    }
+}
